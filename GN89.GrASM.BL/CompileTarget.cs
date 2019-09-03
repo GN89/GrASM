be3559702033397adb79825338dd851593dc293d
German Nemcov <germannemcov@outlook.com>
2019-09-03 20:05:54 +0700

    Added class for compile target

---
+++ GN89.GrASM.BL/CompileTarget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GN89.GrASM.BL
+{
+    public enum CompileTarget
+    {
+        BIN,
+        COM,
+        MZ,
+        DPMI,
+        NE,
+        PE32,
+        PE64
+    }
+}
